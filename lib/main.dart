d8f887d6624e3e2d67bb214c3de99d4cc3e6b438
DeonWaju <54560535+DeonWaju@users.noreply.github.com>
2020-08-12 18:37:01 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app_using_bloc/search_page.dart';
+import 'package:flutter_app_using_bloc/weatherRepo.dart';
+import 'package:flutter_app_using_bloc/weather_bloc.dart';
+import 'package:flutter_app_using_bloc/weather_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          resizeToAvoidBottomInset: false,
+          backgroundColor: Colors.grey[900],
+          body: BlocProvider(
+            builder: (context) => WeatherBloc(WeatherRepo()),
+            child: SearchPage(),
+          ),
+        )
+    );
+  }
+}
